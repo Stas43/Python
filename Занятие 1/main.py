ff4a6b2254927eaaa286552b509b74046d0df102
DaniCraft <staskrichmar@gmail.com>
2023-06-13 08:33:20 +0300

    Ex 1

--- Занятие 1/main.py
+++ Занятие 1/main.py
@@ -1,14 +1,13 @@
-def symbolCounterTheBest(s: str):
-    symbols = {}
-    for char in s:
-        symbols[char] = symbols.get(char, 0) + 1
-    print(symbols)
-    # for char, counter in symbols.items():
-    #     print(char, counter)
+def is_palindrome(string):
+    reversed_string = ""
+    for i in range(len(string), 0, -1):
+        reversed_string += string[i - 1]
+        if string == reversed_string:
+            print("True")
+        else:
+            print("False")
 
 
-symbolCounter('aaabbbcccdddddd')
-print('\n')
-symbolCounterBetter('aaabbbcccdddddd')
-print('\n')
-symbolCounterTheBest('aaabbbcccdddddd')
+
+is_palindrome("radar")
+is_palindrome("Python")
